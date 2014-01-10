66a47050a71d903947ad0f4d24053acd7f5a0094
Daniel Bojdo <daniel.bojdo@web-it.eu>
2014-01-10 23:34:50 +0100

    [new] command wrapper

--- lib/Webit/Pdf/PdfLatex/Util.php
+++ lib/Webit/Pdf/PdfLatex/Util.php
@@ -2,6 +2,12 @@
 namespace Webit\Pdf\PdfLatex;
 
 class Util {
+	
+	/**
+	 * 
+	 * @param string $input
+	 * @return string
+	 */
     public static function escapeLatexSpecialChars($input) {
         if (is_string($input)) {
             $search = array('\\', '#', '$', '€', '%', '&', '~', '_', '{', '}', '^', '\\$\backslash\\$');
@@ -12,11 +18,42 @@ class Util {
         return $input;
     }
     
+    /**
+     * 
+     * @param string $input
+     * @return mixed
+     */
     public static function nobreakSpace($input) {
         return str_replace(' ', '~', $input);
     }
     
+    /**
+     * 
+     * @param string $input
+     * @return string
+     */
     public static function newLine($input) {
         return preg_replace('/\n/', '\\\\\\\\', $input);
+    }
+    
+    /**
+     * 
+     * @param string $input
+     * @param string $command
+     * @param array $options
+     * @return string
+     */
+    public static function command($input, $command, array $options = array()) {
+    	$strOptions = '';
+    	if(count($options) > 0) {
+    		$arOptions = array();
+    		foreach($options as $key => $value) {
+    			$arOptions[] = $key.'='.$value;
+    		}
+    		$strOptions = sprintf('[%s]',implode(',',$arOptions));
+    	}
+    	$wrapped = sprintf('\%s%s{%s}',$command, $strOptions, self::escapeLatexSpecialChars($input));
+    	
+    	return $wrapped;
     }
 }
